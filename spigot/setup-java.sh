b07e955a30213213b22c2cf34ec7a11130edc9a1
Ethan P <32112321+eth-p@users.noreply.github.com>
2019-04-20 19:06:41 -0700

    Update setup-java.sh

--- spigot/setup-java.sh
+++ spigot/setup-java.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+# ------------------------------------------------------------------------------
 set -e
+source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
+# ------------------------------------------------------------------------------
 
 # Use Alpine OpenJDK 8
+build_status "Installing OpenJDK 8..."
+build_note SLOW_DOWNLOAD
 apk add nss
 apk add openjdk8
 apk add openjdk8-jre
