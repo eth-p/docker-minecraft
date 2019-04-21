fea0d7a62385adef06830abd420a6e27588b629c
Ethan P <32112321+eth-p@users.noreply.github.com>
2019-04-20 20:53:14 -0700

    Update setup-finalize.sh

--- image/setup-finalize.sh
+++ image/setup-finalize.sh
@@ -4,6 +4,15 @@ set -e
 source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 # ------------------------------------------------------------------------------
 
+build_status "Enabling tools at /mc/bin"
+
+for tool in /mc/bin/*; do
+	if [[ "$tool" =~ \.sh$ ]]; then
+		chmod +x "$tool"
+		mv "$tool" "$(dirname "$tool")/$(basename "$tool" ".sh")"
+	fi
+done
+
 build_status "Cleaning up..."
 
 rm -rf /var/cache/apk/*
