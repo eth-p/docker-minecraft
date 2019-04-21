a1b36099aff475e0a54c5fb6eba798bb50f08fcf
Ethan P <32112321+eth-p@users.noreply.github.com>
2019-04-20 19:07:57 -0700

    Update setup-tools.sh

--- spigot/setup-tools.sh
+++ spigot/setup-tools.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
+# ------------------------------------------------------------------------------
+set -e
+source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
+# ------------------------------------------------------------------------------
+
+build_status Installing system tools and libraries...
+build_note   SLOW_DOWNLOAD
+
 apk upgrade --update
 apk add --update bash curl git ca-certificates
 
