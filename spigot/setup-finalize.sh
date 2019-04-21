7b4ebc391cf157d61a253ad1fd4549b6a3c2c037
Ethan P <32112321+eth-p@users.noreply.github.com>
2019-04-20 19:08:25 -0700

    Update setup-finalize.sh

--- spigot/setup-finalize.sh
+++ spigot/setup-finalize.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+# ------------------------------------------------------------------------------
 set -e
+source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
+# ------------------------------------------------------------------------------
+
+build_status "Cleaning up..."
 
 rm -rf /var/cache/apk/*
-rm -rf /tmp/*
+rm -rf /build/*
