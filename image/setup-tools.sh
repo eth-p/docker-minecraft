55cccb198796f6c6a27e8af5e2ed70d3db52f2d7
Ethan P <32112321+eth-p@users.noreply.github.com>
2020-03-01 22:50:35 -0800

    Add edge/community repo for openjdk11

--- image/setup-tools.sh
+++ image/setup-tools.sh
@@ -2,5 +2,11 @@
 # Note: Bash is not installed yet.
 # ------------------------------------------------------------------------------
 
+# Add edge/community repository.
+echo "http://nl.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
+
+# Update repository cache.
 apk upgrade --update
+
+# Install tools.
 apk add --update bash curl git ca-certificates
